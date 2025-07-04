df24715337288c2c4764a55f0a4b05874b065a4d
denverma7 <126649187+denverma7@users.noreply.github.com>
2025-07-04 20:29:12 +0500

    change Login and Signup pages

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,7 +13,7 @@ export default function Login({ setToken }){
         setAuthError("");
 
         // const response = await fetch("http://localhost:8080/login" , {
-        const response = await fetch("https://to-do-backend-puce.vercel.app/login" , {
+        const response = await fetch("https://to-do-backend-8b0f2x0hl-denverma7-gmailcoms-projects.vercel.app/" , {
             method: "POST",
             headers: { "Content-Type" : "application/json"},
             body: JSON.stringify({username, password}),
