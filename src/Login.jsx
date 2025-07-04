04919611e4ac114a87e37172d68bd69be5f29a70
denverma7 <126649187+denverma7@users.noreply.github.com>
2025-07-04 19:09:54 +0500

    Updated login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,7 +13,7 @@ export default function Login({ setToken }){
         setAuthError("");
 
         // const response = await fetch("http://localhost:8080/login" , {
-        const response = await fetch("https://to-do-backend-puce.vercel.app//login" , {
+        const response = await fetch("https://to-do-backend-puce.vercel.app/login" , {
             method: "POST",
             headers: { "Content-Type" : "application/json"},
             body: JSON.stringify({username, password}),
